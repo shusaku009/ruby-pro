0c6a5feed0271033bdaf398eb1eecfb1a10f8243
shusaku009 <shusaku009@gmail.com>
2022-08-19 10:27:03 +0900

    method.rbファイルを更新

--- method/method.rb
+++ method/method.rb
@@ -85,3 +85,143 @@ charge(25)
 
 numbers = (1..4).to_a
 
+def greet(*names)
+  "#{names.join('と')},こんにちは！"
+end
+greet('田中さん')
+greet('田中さん','鈴木さん')
+greet('田中さん','鈴木さん','佐藤さん')
+
+def foo(a, *b, c, d)
+  puts = "a = #{a}, b = #{b}, c = #{c}, d = #{d}"
+end
+
+foo(1,2,3,4,5,6)
+
+a = [1,2,3]
+
+[a]
+
+[*a]
+
+a, *b, c, d = 1,2,3,4,5
+
+fruits = ['apple', 'orange', 'melon']
+
+fruits.each_with_index { |fruit, i| puts "#{i}:#{fruit}"}
+
+fruits = ['apple', 'orange', 'melon']
+
+fruits.map.with_index { |fruit,i| "#{i}: #{fruit}" }
+
+fruits = ['apple', 'orange', 'melon']
+
+fruits.delete_if.with_index { |fruit,i| fruit.include?('a')&& i.odd? }
+
+fruits = ['apple', 'orange', 'melon']
+
+p fruits.each
+p fruits.map
+p fruits.delete_if
+
+fruits = ['apple', 'orange','melon']
+
+fruits.each.with_index(1) { |fruit,i| puts "#{i}: #{fruit}"}
+
+dimensions = [
+  [10,20],
+   [30,40],
+   [50,60],
+]
+
+areas = []
+
+dimensions.each do |dimension|
+  length = dimension[0]
+  width = dimension[1]
+  areas << length * width
+end
+areas
+
+dimensions.each do |length,width|
+  areas << length * width
+end
+areas
+
+dimensions.each do |length,width,foo,bar|
+  p [length,width,foo,bar]
+end
+
+dimensions = [
+  [10,20,100],
+   [30,40,200],
+   [50,60,300],
+]
+
+dimensions.each do | length, width|
+  p [length,width]
+end
+
+
+# dimensions.each do | length, width i|
+#   puts "length: #{length}, width: #{width}, i: #{i}"
+# end
+
+
+dimensions = [
+  [10,20],
+   [30,40],
+   [50,60],
+]
+dimensions.each_with_index do |length, width, i|
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+
+dimensions = [
+  [10,20],
+   [30,40],
+   [50,60],
+]
+dimensions.each_with_index do |dimension,i|
+  length = dimension[0]
+  width = dimension[1]
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+dimensions = [
+  [10,20],
+   [30,40],
+   [50,60],
+]
+dimensions.each_with_index do |(length,width),i|
+  puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+dimension,i = [[10,20],0]
+dimension
+i
+
+(length,width), i = [[10,20],0]
+length
+width
+i
+
+['1','20','300'].map { |s| s.rjust(3,'0') }
+
+['1','20','300'].map { _1.rjust(3,'0') }
+
+
+numbers = [1,2,3,4]
+sum = 0
+numbers.each do |n; sum|
+  sum = 10
+  sum += n
+  puts sum
+end
+
+File.open('./sample.txt','w') do |file|
+  file.puts ('1行目のテキストです')
+  file.puts ('2行目のテキストです')
+  file.puts ('3行目のテキストです')
+end
